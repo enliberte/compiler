3f99a678aefcd7f0c55515cb7f6ce3365aabd24e
Alexey Sumatokhin <Alexey.Sumatokhin@confirmit.com>
2021-10-19 11:06:58 +0300

    lexer: handle special symbols

---
+++ lexer/lexer_test.go
@@ -0,0 +1,39 @@
+package lexer
+
+import (
+	"testing"
+
+	"compiler/token"
+)
+
+func TestNextToken(t *testing.T)  {
+	input := `=+(){},;`
+
+	tests := []struct{
+		expectedType token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LBRACE, "("},
+		{token.RBRACE, ")"},
+		{token.LPAREN, "{"},
+		{token.RPAREN, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+	}
+}
