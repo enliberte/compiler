1ef27d4b15e8932f5b1597f5171a4764392b6b41
Alexey Sumatokhin <Alexey.Sumatokhin@confirmit.com>
2021-10-25 13:35:23 +0300

    eval: integer evaluation

--- repl/repl.go
+++ repl/repl.go
@@ -2,6 +2,7 @@ package repl
 
 import (
 	"bufio"
+	"compiler/evaluator"
 	"compiler/lexer"
 	"compiler/parser"
 	"fmt"
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer)  {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
